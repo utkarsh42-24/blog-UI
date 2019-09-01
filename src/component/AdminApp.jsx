cbd7bc5376905f7d29dd7bc58767f440b6182598
Utkarsh Singh <ut42@Utkarshs-MacBook-Pro.local>
2019-09-01 23:48:29 +0530

    creating an Application Component

---
+++ src/component/AdminApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdminApp extends Component {
+    render() {
+        return (
+              <h1>Admin Application</h1>
+        )
+    }
+}
+
+export default AdminApp;
